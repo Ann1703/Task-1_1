7518d2adb91fac9da0f5065c54f56d60294d5f83
Ann1703 <79947765+Ann1703@users.noreply.github.com>
2023-10-09 16:14:16 +0300

    Add files via upload

---
+++ Task2.c
@@ -0,0 +1,41 @@
+
+#define EXIT_SUCCESS /*implementation defined*/
+#define EXIT_FAILURE /*implementation defined*/
+
+#include <stdio.h>
+
+/**
+* @breaf Перевод введеного значения из грамм в унции 
+* @param weight значение в граммах
+* @param ounce значение константы ounce
+* @return Возвращение  результата в унциях
+*/
+double get_ounce(double weight);
+/** @breaf  точка входа в программу
+	@return EXIT_FAILURE если неверный ввод
+	@return EXIT_SUCCESS если ввод верный 
+*/
+  int main()
+{ 
+	double weight =0;
+	double result;
+    result= get_ounce(weight);
+	printf_s("% s", "Weight=");
+	if (scanf_s("%lf", &weight) != 1)
+	{
+		printf_s("%s","Wrong value");
+		return EXIT_FAILURE
+		abort();
+
+	}
+	printf_s("weight = %lf", weight);
+	printf_s("\nresult=%lf", result);
+	return EXIT_SUCCESS;
+  }
+  double get_ounce(double weight)
+	{
+		
+		const double ounce = 28.3;
+		return  weight * ounce;
+    }
+	
