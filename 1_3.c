a43e77fc9267343941d121300084524345f72b2b
Ann1703 <79947765+Ann1703@users.noreply.github.com>
2023-10-10 11:55:21 +0300

    Add files via upload

---
+++ 1_3.c
@@ -0,0 +1,53 @@
+﻿#define EXIT_SUCCESS /*implementation defined*/
+#define EXIT_FAILURE /*implementation defined*/
+
+#include <stdio.h>
+/**
+* @breaf Расчёт работы электрического тока
+* @param voltage значение напряжения в вольтах
+* @param current_strenght значения силы тока в амперах
+* @param work_current значение работы электрического тока
+* @return Вывод полученного результата
+*/
+double get_result(int voltage, double  current_strenght);
+/** @breaf  точка входа в программу
+	@return EXIT_FAILURE если неверный ввод
+	@return EXIT_SUCCESS если ввод верный
+*/
+
+int main()
+{
+	int voltage = 0;
+	double  current_strenght = 0.0;
+	int work_current = 0;
+		
+	printf_s("% s", "Voltage=");
+	
+	if (scanf_s("%d", &voltage) != 1 && scanf_s("%lf", &current_strenght) != 1)
+	{
+		printf_s("%s", "Wrong value");
+		return EXIT_FAILURE;
+			abort();
+
+	}
+	printf_s("\n% s", "Current_strenght=");
+	if (scanf_s("%lf", &current_strenght) != 1)
+	{
+		printf_s("%s", "Wrong value");
+		return EXIT_FAILURE;
+		abort();
+
+	}
+	printf_s("voltage = %d\n", voltage);
+	printf_s("current_strenght = %lf\n", current_strenght);
+	work_current = get_result(voltage, current_strenght);
+	printf_s("\nwork_current=%d", work_current);
+	return EXIT_SUCCESS;
+}
+double get_result(int voltage, double current_strenght)
+{
+
+	const int timeMin = 20;
+	int toSec = 60;
+	return  voltage * current_strenght * timeMin * toSec;
+}
