cbbfccd1a5d39c6e20b490eabf27023619cdc94a
Ann1703 <79947765+Ann1703@users.noreply.github.com>
2023-10-16 10:27:41 +0300

    Update 1_3.c

--- 1_3.c
+++ 1_3.c
@@ -1,4 +1,4 @@
-﻿#define EXIT_SUCCESS /*implementation defined*/
+#define EXIT_SUCCESS /*implementation defined*/
 #define EXIT_FAILURE /*implementation defined*/
 
 #include <stdio.h>
@@ -9,34 +9,29 @@
 * @param work_current значение работы электрического тока
 * @return Вывод полученного результата
 */
-double get_result(int voltage, double  current_strenght);
+double get_result(double voltage, double  current_strenght);
+
 /** @breaf  точка входа в программу
 	@return EXIT_FAILURE если неверный ввод
 	@return EXIT_SUCCESS если ввод верный
 */
-
 int main()
 {
-	int voltage = 0;
+	double voltage = 0.0;
 	double  current_strenght = 0.0;
-	int work_current = 0;
-		
+	double work_current = 0.0;
 	printf_s("% s", "Voltage=");
 	
 	if (scanf_s("%d", &voltage) != 1 && scanf_s("%lf", &current_strenght) != 1)
 	{
 		printf_s("%s", "Wrong value");
 		return EXIT_FAILURE;
-			abort();
-
 	}
 	printf_s("\n% s", "Current_strenght=");
 	if (scanf_s("%lf", &current_strenght) != 1)
 	{
 		printf_s("%s", "Wrong value");
 		return EXIT_FAILURE;
-		abort();
-
 	}
 	printf_s("voltage = %d\n", voltage);
 	printf_s("current_strenght = %lf\n", current_strenght);
@@ -44,9 +39,8 @@ int main()
 	printf_s("\nwork_current=%d", work_current);
 	return EXIT_SUCCESS;
 }
-double get_result(int voltage, double current_strenght)
+double get_result(double voltage, double current_strenght)
 {
-
 	const int timeMin = 20;
 	int toSec = 60;
 	return  voltage * current_strenght * timeMin * toSec;
